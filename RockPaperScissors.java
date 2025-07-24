fe1f201b72661a4ba6e80bdf2536dbf1263fbf71
ganapathithanuja <ganapathithanuja62@gmail.com>
2025-07-24 10:15:46 +0530

    Create RockPaperScissors.java

---
+++ RockPaperScissors.java
@@ -0,0 +1,41 @@
+mport java.util.Random;
+import java.util.Scanner;
+
+public class stone{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        String[] choices = {"Rock", "Paper", "Scissors"};
+        
+        System.out.println("Welcome to Rock, Paper, Scissors game!");
+        System.out.println("Enter your choice: Rock, Paper or Scissors.");
+        
+        System.out.print("Your choice: ");
+        String userChoice = scanner.nextLine();
+        
+
+        while (!userChoice.equalsIgnoreCase("Rock") && 
+               !userChoice.equalsIgnoreCase("Paper") && 
+               !userChoice.equalsIgnoreCase("Scissors")) {
+            System.out.println("Invalid input. Please choose Rock, Paper, or Scissors.");
+            System.out.print("Your choice: ");
+            userChoice = scanner.nextLine();
+        }
+
+        int computerChoiceIndex = random.nextInt(3);
+        String computerChoice = choices[computerChoiceIndex];
+        System.out.println("Computer chose: " + computerChoice);
+
+        if (userChoice.equalsIgnoreCase(computerChoice)) {
+            System.out.println("It's a tie!");
+        } else if ((userChoice.equalsIgnoreCase("Rock") && computerChoice.equalsIgnoreCase("Scissors")) ||
+                   (userChoice.equalsIgnoreCase("Paper") && computerChoice.equalsIgnoreCase("Rock")) ||
+                   (userChoice.equalsIgnoreCase("Scissors") && computerChoice.equalsIgnoreCase("Paper"))) {
+            System.out.println("You win!");
+        } else {
+            System.out.println("You lose!");
+        }
+
+        scanner.close();
+    }
+}
